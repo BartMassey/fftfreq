81ed760d1590d0c900a4955b7f88938a24c05f62
Bart Massey <bart.massey@gmail.com>
2024-04-05 21:40:52 -0700

    added non-graphic output, changed w to n for consistency with docs

--- cos.py
+++ cos.py
@@ -4,9 +4,10 @@ from scipy.fft import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--correction", help="Frequency correction", action="store_true")
+ap.add_argument("--plot", help="Output table for plotting", action="store_true")
 ap.add_argument("--sr", help="Sample rate", type=int, default=24000)
 ap.add_argument("--tf", help="Target frequency", type=int, default=250)
-ap.add_argument("--w", help="Window size", type=int, default=512)
+ap.add_argument("--n", help="Window size", type=int, default=512)
 args = ap.parse_args()
 
 # Imputed sample rate in samples/sec.
@@ -15,35 +16,39 @@ sr = args.sr
 tf = args.tf
 assert 2 * tf < sr
 # Window size in samples.
-w = args.w
+n = args.n
 
 # Get a Nyquist target frequency.
 ntf = 2 * tf / sr
 # Get the closest FFT bin index.
-b = round(ntf * w / 2)
+b = round(ntf * n / 2)
 # Get the bin center frequency.
-freqs = fftfreq(w, d=1/sr)
+freqs = fftfreq(n, d=1/sr)
 f0 = freqs[b]
-assert freqs[b] == -freqs[w - b]
+assert freqs[b] == -freqs[n - b]
 
 if args.correction:
     # Correct the center frequency for time-domain cos().
-    fc = f0 * (w - 1) / w
+    fc = f0 * (n - 1) / n
 else:
     fc = f0
 
 # Generate time-domain cos().
-t = np.linspace(0, w / sr, w, dtype=np.float64)
+t = np.linspace(0, n / sr, n, dtype=np.float64)
 y0 = np.cos(2 * np.pi * fc * t)
 
 # Generate FFT cos().
-f = np.zeros(w, dtype=np.complex128)
+f = np.zeros(n, dtype=np.complex128)
 f[b] = 1.0
-f[w - b] = 1.0
-y = 0.5 * w * ifft(f)
+f[n - b] = 1.0
+y = 0.5 * n * ifft(f)
 
 # Show difference.
 assert len(y) == len(y0)
-for i in range(len(y)):
-    print(i / sr, y0[i] - y[i].real)
-#    print(i / sr, abs(y[i].real) - np.abs(y[i]))
+if args.plot:
+    for i in range(len(y)):
+        print(i / sr, y0[i] - y[i].real)
+    #   print(i / sr, abs(y[i].real) - np.abs(y[i]))
+else:
+    e = np.max(np.abs(y0 - np.real(y)))
+    print(e)
--- plotcos.sh
+++ plotcos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 TMP=/tmp/plotcos.$$
 trap "rm -f $TMP" 0 1 2 3 15
-python3 cos.py "$@" > $TMP
+python3 cos.py --plot "$@" > $TMP
 echo "plot \"$TMP\" with lines;" | gnuplot -p
